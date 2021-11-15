f7c0249cd9361e7397de9677b3b21a29a7eaae7d
croutonn <croutonna@gmail.com>
2021-11-16 02:28:34 +0900

    Fix the level of headings in Markdown to one level lower in HTML

--- pages/blog/[pid].tsx
+++ pages/blog/[pid].tsx
@@ -16,14 +16,16 @@ type BlogProps = {
   meta: Record<string, unknown>;
 };
 
+type HTMLElementProps = JSX.IntrinsicAttributes & {
+  children?: React.ReactNode;
+};
+
 const components: Record<string, React.ReactNode> = {
-  Test: function () {
-    return (
-      <>
-        <p>This is a Test Component.</p>
-      </>
-    );
-  },
+  h1: (props: HTMLElementProps) => <h2 {...props}>{props.children}</h2>,
+  h2: (props: HTMLElementProps) => <h3 {...props}>{props.children}</h3>,
+  h3: (props: HTMLElementProps) => <h4 {...props}>{props.children}</h4>,
+  h4: (props: HTMLElementProps) => <h5 {...props}>{props.children}</h5>,
+  h5: (props: HTMLElementProps) => <h6 {...props}>{props.children}</h6>,
 };
 
 const Blog: NextPage<BlogProps> = ({ source }) => {
