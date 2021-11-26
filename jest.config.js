b0b79ce60e49972a5385cf8d4c731982b11c1773
croutonn <croutonna@gmail.com>
2021-11-26 16:44:33 +0900

    Improve the testing environment

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,42 @@
 module.exports = {
-  testEnvironment: "jest-environment-jsdom",
-  // clearMocks: true,
-  // collectCoverage: true,
-  // coverageDirectory: "coverage",
-  // coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  clearMocks: true,
+  coverageDirectory: "tests/coverage",
   collectCoverageFrom: [
     "**/*.{js,jsx,ts,tsx}",
     "!**/*.d.ts",
+    "!<rootDir>/*.js",
+    "!<rootDir>/tests/coverage/**/*",
+    "!<rootDir>/public/**/*",
+    "!<rootDir>/.next/**/*",
     "!**/node_modules/**",
   ],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
   moduleNameMapper: {
     "^.+\\.module\\.css$": "identity-obj-proxy",
-    "^.+\\.css$": "<rootDir>/__mocks__/styleMock.js",
-    "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$": `<rootDir>/__mocks__/fileMock.js`,
+    "^.+\\.css$": "<rootDir>/tests/mocks/styleMock.js",
+    "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$": `<rootDir>/tests/mocks/fileMock.js`,
     "^@/(.*)$": "<rootDir>/$1",
   },
   moduleDirectories: ["node_modules", __dirname],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+    "<rootDir>/public/",
+    "<rootDir>/tests/coverage/",
+  ],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
   transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.css$"],
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "tests/",
+        filename: "report.html",
+      },
+    ],
+  ],
 };
